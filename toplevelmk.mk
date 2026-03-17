a07d1fbd6d9d7acc4bdca3a4a7aa642c74df5193
human23 <msnielsen23@gmail.com>
2026-03-17 19:43:14 +0100

    Create toplevelmk.mk

---
+++ toplevelmk.mk
@@ -0,0 +1,18 @@
+include build/wm.mk
+
+.PHONY: _init build install test clean
+
+_init:
+	mkdir -p $(OUT_DIR)
+
+build: _init wm_build
+
+install: build
+	sudo ./install.sh
+
+test: build
+	Xephyr -ac -screen 1280x720 -br -reset :100 &
+	sleep 1
+	DISPLAY=:100 $(OUT_DIR)/mirawm
+
+clean: clean_wm
